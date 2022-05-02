e7bdc23e6be7b892b1f31f3e90e3a3a9cbb37bc6
Pablo Monardo <pablomonardo@gmail.com>
2022-05-02 19:00:38 -0300

    Arregle funcion que determina que dia fue una fecha y corrijo algunos erroes de tipeo.

--- Fechas.c
+++ Fechas.c
@@ -13,7 +13,11 @@ int determinarBisiesto(const unsigned int year);
 void diaSemana();
 
 int main(){
-
+    tFecha fecha;
+    //fecha = crearFecha(0);
+    //printf("La fecha es %02d/%02d/%d\n", fecha.dia, fecha.mes, fecha.year);
+    diaSemana();
+    system("pause");
     return 0;
 }
 
@@ -35,64 +39,65 @@ int validarFecha(tFecha fecha, int AMinimo) {
     if(fecha.year > AMinimo) {
         esBisiesto = determinarBisiesto(fecha.year);
     } else {
-        printf("Error en el anio.");
+        printf("Error en el anio.\n");
         return 1;
     }
     if(fecha.mes > 12 || fecha.mes < 1){
-        printf("Mes inexistente.");
+        printf("Mes inexistente.\n");
         return 1;
     }
     if(esBisiesto == 0){
         if(fecha.dia < 1 || fecha.dia > diasBisiesto[fecha.mes]){
-            printf("Ingreso de dias invalido, el mes %d tiene %d dias.", fecha.mes, diasBisiesto[fecha.mes]);
+            printf("Ingreso de dias invalido, el mes %d tiene %d dias.\n", fecha.mes, diasBisiesto[fecha.mes]);
             return 1;
         }
     } else
     {
-        if (fecha.mes < 1 || fecha.mes > diasNoBisiesto[fecha.mes])
+        if (fecha.dia < 1 || fecha.dia > diasNoBisiesto[fecha.mes])
         {
-            printf("Ingreso de dias invalido, el mes %d tiene %d dias.", fecha.mes, diasNoBisiesto[fecha.mes]);
+            printf("Ingreso de dias invalido, el mes %d tiene %d dias.\n", fecha.mes, diasNoBisiesto[fecha.mes]);
             return 1;
         }
-        
-    }
-    
 
+    }
+      printf("Fecha valida.\n");
+      return 0;
 }
 
 void diaSemana() {
-    const int t[] = {0,3,2,5,0,3,5,1,4,6,2,4};
+    const int t[] = {0,0,3,2,5,0,3,5,1,4,6,2,4};
     tFecha fecha;
     int diaFinal;
 
-    
+
     printf("Ingresar una fecha posterior al 1752.\n");
     fecha = crearFecha(1752);
-    
 
-    diaFinal = (fecha.year + fecha.year/4 + fecha.year/400 - fecha.year/100 + t[fecha.mes -1] + fecha.dia) % 7;
+
+    diaFinal = (fecha.year + fecha.year/4 + fecha.year/400 - fecha.year/100 + t[fecha.mes] + fecha.dia) % 7;
     switch (diaFinal)
     {
     case 0:
-        printf("El %02d/%02d%d fue domingo",fecha.dia,fecha.mes,fecha.year);
+        printf("El %02d/%02d/%d fue domingo",fecha.dia,fecha.mes,fecha.year);
         break;
     case 1:
-        printf("El %02d/%02d%d fue lunes",fecha.dia,fecha.mes,fecha.year);
+        printf("El %02d/%02d/%d fue lunes",fecha.dia,fecha.mes,fecha.year);
         break;
     case 2:
-        printf("El %02d/%02d%d fue martes",fecha.dia,fecha.mes,fecha.year);
+        printf("El %02d/%02d/%d fue martes",fecha.dia,fecha.mes,fecha.year);
         break;
     case 3:
-        printf("El %02d/%02d%d fue miercoles",fecha.dia,fecha.mes,fecha.year);
+        printf("El %02d/%02d/%d fue miercoles",fecha.dia,fecha.mes,fecha.year);
         break;
     case 4:
-        printf("El %02d/%02d%d fue jueves",fecha.dia,fecha.mes,fecha.year);
+        printf("El %02d/%02d/%d fue jueves",fecha.dia,fecha.mes,fecha.year);
         break;
     case 5:
-        printf("El %02d/%02d%d fue viernes",fecha.dia,fecha.mes,fecha.year);
+        printf("El %02d/%02d/%d fue viernes",fecha.dia,fecha.mes,fecha.year);
         break;
     case 6:
-        printf("El %02d/%02d%d fue sabado",fecha.dia,fecha.mes,fecha.year);
+        printf("El %02d/%02d/%d fue sabado",fecha.dia,fecha.mes,fecha.year);
         break;
     }
+    printf("\n");
 }
