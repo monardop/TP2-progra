de6b0dc6105417151ae368cdb59f1186ab7821f9
Pablo Monardo <pablomonardo@gmail.com>
2022-05-01 21:02:49 -0300

    Creacion de constantes y estructura para la validacion y creacion de fechas

--- Fechas.c
+++ Fechas.c
@@ -1,8 +1,40 @@
 #include <stdlib.h>
+#include <stdio.h>
 
+typedef struct {
+    int dia, mes, year;
+}tFecha;
+const int diasBisiesto[] = {0,31,29,31,30,31,30,31,31,30,31,30,31};
+const int diasNoBisiesto[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 
+tFecha crearFecha();
+int validarFecha(tFecha fecha);
+int determinarBisiesto(const unsigned int year);
+void diaSemana();
 
-void diaSemana () {
+int main(){
+
+    return 0;
+}
+
+tFecha crearFecha() {
+    tFecha fecha;
+    int fechaValida;
+    do {
+        printf("Insertar una fecha de formato dd/mm/aaaa: ");
+        scanf("%d/%d/%d", &fecha.dia, &fecha.mes, &fecha.year);
+        fechaValida = validarFecha(fecha);
+    }while(fechaValida == 1);
+    return fecha;
+}
+int determinarBisiesto(const unsigned int year) {
+    return (year %4 == 0 && (year %100 == 0 || year %400 == 0))?0:1;
+}
+int validarFecha(tFecha fecha) {
+
+}
+
+void diaSemana() {
     const int t[] = {0,3,2,5,0,3,5,1,4,6,2,4};
     tFecha fecha;
     int diaFinal;
